2530ef2069a97518904c63dd8ac35ec0aa902c7c
chingyzhang <chingyzhang@163.com>
2016-06-28 14:22:52 +0800

    添加了打印日志方法

--- Logger/LoggerWriter.java
+++ Logger/LoggerWriter.java
@@ -1,11 +1,30 @@
+import java.awt.image.DirectColorModel;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+
+import javax.naming.spi.DirectoryManager;
+
 public class LoggerWriter {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("123");
-	}
+		try {
+			File file = new File("C://JavaLog//Log.txt");
+			if (!file.getParentFile().exists()) {
+				file.getParentFile().mkdir();
+			}
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file, true);
+			fw.write("你好么？我是Java的打印方法");
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
+	}
 }
